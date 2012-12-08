1f6b7074167ef16f030835cc437c508456f12565
Jarred Kozlick <jkozlick@gmail.com>
2012-12-08 12:20:00 -0700

    adjusted seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,46 +18,46 @@
 Course.delete_all
 Course.create(title: 'ECE314 Signals and Systems',
   description: 
-    %{<p>
+    %{
         Development of analytical and computer-based skills for the analysis and
 synthesis of signals, linear systems, and communications.
-      </p>},  
+      },  
   credit: 3)
 # . . .
 Course.create(title: 'ECE432 Introduction to Parallel Processing',
   description: 
-    %{<p>
+    %{
         Machine taxonomy and introduction to parallel programming. 
 		Performance issues, speed-up, and efficiency. Interconnection networks and embeddings. 
 		Parallel programming issues and models: control parallel, data parallel, and data flow. 
 		Programming assignments on massively parallel machines.
-      </p>},  
+      },  
   credit: 3)
 # . . .
 
 Course.create(title: 'ECE442 Wireless Communications',
   description: 
-    %{<p>
+    %{
         The course is an introduction to cellular telephone systems and wireless networks, drawing upon a diversity of electrical engineering areas. 
 		Topics include cellular concepts, radio propagation, modulation methods and multiple access techniques.
-      </p>},
+      },
   credit: 3)
   
 Program.delete_all
 Program.create(title: 'Computer Engineering',
   description: 
-    %{<p>
+    %{
         ECE Computer Engineering
-      </p>},
+      },
   dept: 'ECE',
   degree: 'MS',
   year: 2012)
 # . . .
 Program.create(title: 'Electrical Engineering',
   description: 
-    %{<p>
+    %{
         ECE Electrical Engineering
-      </p>},
+      },
   dept: 'ECE',
   degree: 'BS',
   year: 2012)
